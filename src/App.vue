18a04a448bd4eaf9b80ef579eb0a271af4249251
MaxAvila <maxvilam@hotmail.con>
2022-05-06 21:19:04 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input
+      v-model="tituloetuiqueta"
+      placeholder="escribe un nombre"
+      type="text"
+    />
+    <div class="clase">
+      <div class="conteiner">
+        <row>
+          <col-12>
+            <TableDat :superdata="trabajadores" />
+          </col-12>
+        </row>
+      </div>
+    </div>
+    <CardsPrueba :infod="tituloetuiqueta" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TableDat from "@/components/TableDat.vue";
+import CardsPrueba from "@/components/CardsPrueba.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      tituloetuiqueta: "QQQQQQQQQQQQQ",
+      ndata: "",
+      trabajadores: [
+        {
+          nombre: "Franco Urra",
+          edad: 47,
+          rut: "12345678-9",
+          diagnostico: "Hipertensión Arterial",
+        },
+        {
+          nombre: "Matías Mesa",
+          edad: 27,
+          rut: "12345678-9",
+          diagnostico: "Diabetes Mellitus 1",
+        },
+        {
+          nombre: "Enrique Flores",
+          edad: 37,
+          rut: "12345678-9",
+          diagnostico: "Disfunción de la vista",
+        },
+        {
+          nombre: "Milagros Acosta",
+          edad: 17,
+          rut: "12345678-9",
+          diagnostico: "Hipertensión Pulmonar",
+        },
+        {
+          nombre: "Victor Fernandez",
+          edad: 47,
+          rut: "12345678-9",
+          diagnostico: "Diabetes Mellitus 2",
+        },
+        {
+          nombre: "Is Pinto",
+          edad: 15,
+          rut: "12345678-9",
+          diagnostico: "Pancreatitis Aguda",
+        },
+        {
+          nombre: "Francisco Lobos",
+          edad: 2,
+          rut: "12345678-9",
+          diagnostico: "Neuroblastoma",
+        },
+        {
+          nombre: "Jean Jaque",
+          edad: 67,
+          rut: "12345678-9",
+          diagnostico: "Enfermedad de Parkinson",
+        },
+        {
+          nombre: "Cesar Riveros",
+          edad: 87,
+          rut: "12345678-9",
+          diagnostico: "Alopecia por Radiación",
+        },
+        {
+          nombre: "Joshua Espinoza",
+          edad: 77,
+          rut: "12345678-9",
+          diagnostico: "Enfermedad de Creutzfeldt-Jakob",
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    TableDat,
+    CardsPrueba,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/TableDat.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">nombre</th>
+        <th scope="col">rut</th>
+        <th scope="col">diagnostico</th>
+      </tr>
+    </thead>
+    <tbody v-if="superdata.length > 0">
+      <tr v-for="(trabajador, i) in superdata" :key="i">
+        <th>{{ i + 1 }}</th>
+        <td>{{ trabajador.nombre }}</td>
+        <td>{{ trabajador.rut }}</td>
+        <td>@{{ trabajador.diagnostico }}</td>
+      </tr>
+    </tbody>
+    <tbody v-else>
+      <tr>
+        <td colspan="4">sin data</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "TableDat",
+  props: {
+    superdata: {
+      type: Array,
+      default: () => [] /* el valor por defecto */,
+    },
+  },
+};
+</script>
+
+<style></style>
